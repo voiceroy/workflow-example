ccc1b8050e9b2cc7d38a4721f03dbc519f1da37b
Harishankar G <harishankargaddanakeri@gmail.com>
2025-04-15 19:12:38 +0530

    Add a new function

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func ReturnHello() string {
 	return "Hello, World!"
 }
 
+func ReturnInteger() int {
+	return rand.Int()
+}
+
 func main() {
 	fmt.Println(ReturnHello())
+	fmt.Printf("Random Integer: %d\n", ReturnInteger())
 }
